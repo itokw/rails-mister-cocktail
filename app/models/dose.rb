5ac86ee0d065d8c5fc4c887c74253c5f2587ade5
Ken Ito <kenwohlmannstetter@hotmail.de>
2018-11-09 16:47:22 +0900

    add images

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,4 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :description, :cocktail, :ingredient, presence: true
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,5 @@
 class Ingredient < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :doses
+  has_many :cocktail, through: :doses
 end
